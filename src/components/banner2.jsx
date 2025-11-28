409ab14466669d6f9c87cb16315d6c012f61c6eb
TheReacher4013 <rushikeshjoshi4013@gmail.com>
2025-11-28 15:47:26 +0530

    adding some details in animation

--- src/components/banner2.jsx
+++ src/components/banner2.jsx
@@ -1,3 +1,33 @@
+// import React from 'react';
+// import { motion } from 'motion/react';
+// import "../css/banner2.css";
+// import libraryImg from "../assets/images/banner2.png";
+
+// const BannerLast = () => {
+//     return (
+//         <section className="library_hero">
+//             <motion.div
+//                 className="library_hero_inner"
+//                 style={{
+//                     backgroundImage: `url(${libraryImg})`,
+//                     backgroundSize: "cover",
+//                     backgroundPosition: "center",
+//                     backgroundRepeat: "no-repeat",
+//                     width: "100%",
+//                     height: "100%",
+//                 }}
+//                 initial={{ opacity: 0, scale: 0.95 }}
+//                 animate={{ opacity: 1, scale: 1 }}
+//                 transition={{ duration: 1.2, ease: "easeOut" }}
+//             ></motion.div>
+//         </section>
+//     );
+// };
+
+// export default BannerLast;
+
+
+
 import React from 'react';
 import { motion } from 'motion/react';
 import "../css/banner2.css";
@@ -16,9 +46,10 @@ const BannerLast = () => {
                     width: "100%",
                     height: "100%",
                 }}
-                initial={{ opacity: 0, scale: 0.95 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 1.2, ease: "easeOut" }}
+                initial={{ opacity: 0, scale: 0.9, y: 40 }}
+                whileInView={{ opacity: 1, scale: 1, y: 0 }}
+                transition={{ duration: 5.3, ease: "easeOut" }}
+                viewport={{ once: false, amount: 0.3 }}
             ></motion.div>
         </section>
     );
--- src/components/whatWeOffer.jsx
+++ src/components/whatWeOffer.jsx
@@ -1,40 +1,126 @@
+// import React from 'react';
+// import { NavLink } from "react-router-dom";
+// import { motion } from "motion/react";
+// import "../css/whatWeOffer.css";
+
+// const WhatWeOffer = () => {
+//     return (
+//         <section className='offer-section'>
+
+//             {/* Left side heading */}
+//             <motion.div
+//                 className='offer-left'
+//                 initial={{ opacity: 0, x: -50 }}
+//                 animate={{ opacity: 1, x: 0 }}
+//                 transition={{ duration: 0.6 }}
+//             >
+//                 <p className='offer-heading'>What We Offer</p>
+//             </motion.div>
+
+//             {/* Right side list */}
+//             <motion.div
+//                 className='offer-right'
+//                 initial={{ opacity: 0, x: 50 }}
+//                 animate={{ opacity: 1, x: 0 }}
+//                 transition={{ duration: 0.6, delay: 0.2 }}
+//             >
+//                 <h1 className='offer-list'>
+//                     Business Formation<br />
+//                     Contract Drafting<br />
+//                     Mergers And Acquisitions<br />
+//                     Property Protection<br />
+//                     Employment Law<br />
+//                     Corporate Governance<br />
+//                     Shareholder Disputes<br />
+//                     <span className='more'>+More</span>
+//                 </h1>
+
+//                 <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
+//                     <NavLink to="/services" className="seri-button">
+//                         Services
+//                     </NavLink>
+//                 </motion.div>
+//             </motion.div>
+
+//         </section>
+//     );
+// };
+
+// export default WhatWeOffer;
+
 import React from 'react';
 import { NavLink } from "react-router-dom";
 import { motion } from "motion/react";
 import "../css/whatWeOffer.css";
 
+const lines = [
+    "Business Formation",
+    "Contract Drafting",
+    "Mergers And Acquisitions",
+    "Property Protection",
+    "Employment Law",
+    "Corporate Governance",
+    "Shareholder Disputes",
+    "+More"
+];
+
 const WhatWeOffer = () => {
     return (
         <section className='offer-section'>
 
-            {/* Left side heading */}
+            {/* LEFT HEADING */}
             <motion.div
                 className='offer-left'
                 initial={{ opacity: 0, x: -50 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ duration: 0.6 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 2.8, ease: "easeOut" }}
+                viewport={{ once: false, amount: 0.3 }}
             >
                 <p className='offer-heading'>What We Offer</p>
             </motion.div>
 
-            {/* Right side list */}
+            {/* RIGHT LIST */}
             <motion.div
                 className='offer-right'
                 initial={{ opacity: 0, x: 50 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ duration: 0.6, delay: 0.2 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 2.8, delay: 2.1, ease: "easeOut" }}
+                viewport={{ once: false, amount: 0.3 }}
             >
-                <h1 className='offer-list'>
-                    Business Formation<br />
-                    Contract Drafting<br />
-                    Mergers And Acquisitions<br />
-                    Property Protection<br />
-                    Employment Law<br />
-                    Corporate Governance<br />
-                    Shareholder Disputes<br />
-                    <span className='more'>+More</span>
-                </h1>
+                <motion.h1
+                    className='offer-list'
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: false, amount: 0.3 }}
+                    variants={{
+                        hidden: {},
+                        visible: {
+                            transition: { staggerChildren: 0.25 } // Slow & ordered
+                        }
+                    }}
+                >
+                    {lines.map((text, i) => (
+                        <motion.span
+                            key={i}
+                            style={{ display: "block" }}
+                            variants={{
+                                hidden: { opacity: 0, y: 40 },
+                                visible: {
+                                    opacity: 1,
+                                    y: 0,
+                                    transition: {
+                                        duration: 1.0, // slower reveal
+                                        ease: "easeOut"
+                                    }
+                                }
+                            }}
+                        >
+                            {text}
+                        </motion.span>
+                    ))}
+                </motion.h1>
 
+                {/* Button */}
                 <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
                     <NavLink to="/services" className="seri-button">
                         Services
